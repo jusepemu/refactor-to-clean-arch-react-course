e27f5d52bd222a6d1b7120c865ae95726fce9bba
jusepemu <juanse9701@hotmail.com>
2025-01-03 22:40:07 -0500

    chore: exec pre commit

--- src/pages/__test__/ProductsPage.spec.tsx
+++ src/pages/__test__/ProductsPage.spec.tsx
@@ -3,7 +3,11 @@ import { test } from "vitest";
 import { AppProvider } from "../../context/AppProvider";
 import { ProductsPage } from "../ProductsPage";
 
-const renderComponent = () => (<AppProvider><ProductsPage /></AppProvider>)
+const renderComponent = () => (
+    <AppProvider>
+        <ProductsPage />
+    </AppProvider>
+);
 
 test("loads and display the title", async () => {
     render(renderComponent());
