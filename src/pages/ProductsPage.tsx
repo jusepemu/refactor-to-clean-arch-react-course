8955e1c81ecebe0f35f1be7819a131853bd9cf47
jusepemu <juanse9701@hotmail.com>
2025-01-16 22:44:12 -0500

    refactor: Move the get and load products to custom hook

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -8,11 +8,11 @@ import {
 import { Footer } from "../components/Footer";
 import { MainAppBar } from "../components/MainAppBar";
 import styled from "@emotion/styled";
-import { ChangeEvent, useCallback, useEffect, useMemo, useState } from "react";
+import { ChangeEvent, useCallback, useMemo, useState } from "react";
 import { useAppContext } from "../context/useAppContext";
 import { ConfirmationDialog } from "../components/ConfirmationDialog";
-import { useReload } from "../hooks/useReload";
-import { RemoteProduct, StoreApi } from "../api/StoreApi";
+import { StoreApi } from "../api/StoreApi";
+import { buildProduct, Product, useProducts } from "./useProducts";
 
 const baseColumn: Partial<GridColDef<Product>> = {
     disableColumnMenu: true,
@@ -23,27 +23,14 @@ const storeApi = new StoreApi();
 
 export const ProductsPage: React.FC = () => {
     const { currentUser } = useAppContext();
-    const [reloadKey, reload] = useReload();
 
-    const [products, setProducts] = useState<Product[]>([]);
     const [snackBarError, setSnackBarError] = useState<string>();
     const [snackBarSuccess, setSnackBarSuccess] = useState<string>();
 
     const [editingProduct, setEditingProduct] = useState<Product | undefined>(undefined);
     const [priceError, setPriceError] = useState<string | undefined>(undefined);
 
-    // FIXME: Load products
-    useEffect(() => {
-        storeApi.getAll().then(response => {
-            console.debug("Reloading", reloadKey);
-
-            const remoteProducts = response as RemoteProduct[];
-
-            const products = remoteProducts.map(buildProduct);
-
-            setProducts(products);
-        });
-    }, [reloadKey]);
+    const { products, reload } = useProducts(storeApi);
 
     // FIXME: Load product
     // FIXME: Validate user is admin
@@ -278,13 +265,6 @@ const ProductImage = styled.img`
 
 type ProductStatus = "active" | "inactive";
 
-export interface Product {
-    id: number;
-    title: string;
-    image: string;
-    price: string;
-}
-
 const StatusContainer = styled.div<{ status: ProductStatus }>`
     background: ${props => (props.status === "inactive" ? "red" : "green")};
     display: flex;
@@ -295,19 +275,6 @@ const StatusContainer = styled.div<{ status: ProductStatus }>`
     border-radius: 20px;
     width: 100px;
 `;
-
-// FIXME: Mapping products
-function buildProduct(remoteProduct: RemoteProduct): Product {
-    return {
-        id: remoteProduct.id,
-        title: remoteProduct.title,
-        image: remoteProduct.image,
-        price: remoteProduct.price.toLocaleString("en-US", {
-            maximumFractionDigits: 2,
-            minimumFractionDigits: 2,
-        }),
-    };
-}
 
 // FIXME: Regex to validate price
 const priceRegex = /^\d+(\.\d{1,2})?$/;
---
+++ src/pages/useProducts.ts
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { RemoteProduct, StoreApi } from "../api/StoreApi";
+import { useReload } from "../hooks/useReload";
+
+export interface Product {
+    id: number;
+    title: string;
+    image: string;
+    price: string;
+}
+
+export function buildProduct(remoteProduct: RemoteProduct): Product {
+    return {
+        id: remoteProduct.id,
+        title: remoteProduct.title,
+        image: remoteProduct.image,
+        price: remoteProduct.price.toLocaleString("en-US", {
+            maximumFractionDigits: 2,
+            minimumFractionDigits: 2,
+        }),
+    };
+}
+
+export function useProducts(storeApi: StoreApi) {
+    const [reloadKey, reload] = useReload();
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        storeApi.getAll().then(response => {
+            console.debug("Reloading", reloadKey);
+
+            const remoteProducts = response as RemoteProduct[];
+
+            const products = remoteProducts.map(buildProduct);
+
+            setProducts(products);
+        });
+    }, [reloadKey, storeApi]);
+
+    return {
+        products,
+        reload,
+    };
+}
